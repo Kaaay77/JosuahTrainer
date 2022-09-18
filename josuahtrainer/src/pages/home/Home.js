d8c9ccec19b5694442714600bc318ea6352278fb
Kaaay77 <96114448+Kaaay77@users.noreply.github.com>
2022-09-18 17:58:33 +0200

    fatloos87

--- josuahtrainer/src/pages/home/Home.js
+++ josuahtrainer/src/pages/home/Home.js
@@ -6,27 +6,28 @@ export default function Home () {
     return(
         <div className="home">
             <header>
-                <div>
-                    <p>ñkjhsDF ahf aSFHjaksfnh asFUa sfÀSOifh adfoDHAF adf aFUahfpauIFADF   AIDUF aif df.</p>
+                <div className="cabecera">
+                    <p>Aqui va la tipica frase de consigue tu cambio bla bla bla</p>
+                    <p>PARA PERSONAS QUE QUIERAN PERDER HASTA 10 KG EN 87 DÍAS, BLABLABLA... :</p>
                 </div>
             </header>
-            <section>
-                <div>
+            <section className="flex-column" >
+                <div className="caja-dark" >
                     <h2>FATLOSS87</h2>
                 </div>
                 <ul>
-                    <li>Planificación entrenamientos durante 87 días.</li>
-                    <li>3-6 entrenamientos por semana.</li>
+                    <li>Planificación entrenamientos durante <b>87 días</b>.</li>
+                    <li><b>3-6</b> entrenamientos por semana.</li>
                     <li>Visualización y registro entrenamientos.</li>
                     <li>Revisión semanal y actualización planificación.</li>
-                    <li>Contacto por chat-Dudyfit.</li>
-                    <li>Grupo conjunto con otros #fatloosers.</li>
+                    <li>Contacto por <b>chat-Dudyfit</b>.</li>
+                    <li>Grupo conjunto con otros <b>#fatloosers</b>.</li>
                     <li>Vídeos explicativos.</li>
-                    <li>1 sesión en directo semanal (según disponibilidad horaria).</li>
+                    <li>1 sesión <b>en directo</b> semanal <br/>(según disponibilidad horaria).</li>
                     <li>Recomendaciones nutricionales básicas.</li>
                 </ul>
-                <div>
-                    <h3><b>279€</b>/pago único</h3>
+                <div className="caja-grey">
+                    <h3><b>279€</b>/pago <br/> único</h3>
                 </div>
             </section>
         </div>
